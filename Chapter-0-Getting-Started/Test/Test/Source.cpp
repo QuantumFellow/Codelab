e7897e28ec5db3f1bccd688c48f4d8a5cc99b470
QuantumFellow <blue.lonie22@bathspa.ac.uk>
2024-03-27 13:25:15 +0000

    Renaming

--- Chapter-0-Getting-Started/Test/Test/Source.cpp
+++
@@ -1,1 +0,0 @@
-//testing to see if this comment stuff works
--- Chapter-0-Getting-Started/Test2/Source.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-
-	cout << "| 1 | 2 | 3 | \n| 4 | 5 | 6 | \n| 7 | 8 | 9 |" << endl;
-}
